5080fee25749e35c68658a94c3091e9e6dd4b9f2
Kamila Zaeva <Kamila_Zaeva@epam.com>
2017-03-06 17:12:08 +0300

    add simple route's block

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
+import {Router, Route, hashHistory} from "react-router"
 import {createStore} from "redux";
 import todoApp from "./reducers/reducers";
 import App from "./components/App";
@@ -8,9 +9,14 @@ import App from "./components/App";
 let store = createStore(todoApp,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(
+const Root = () => (
     <Provider store={store}>
-        <App />
-    </Provider>,
+        <Router history={hashHistory}>
+            <Route path='/(:filter)' component={App} />
+        </Router>
+    </Provider>
+);
+ReactDOM.render(
+    <Root />,
     document.getElementById("root")
 );
